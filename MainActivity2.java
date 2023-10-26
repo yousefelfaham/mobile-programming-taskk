e929a331ffc0c874732be315e86959e1bba9191d
Yousef Hazem Elfaham <102233337+yousefelfaham@users.noreply.github.com>
2023-10-26 23:40:31 +0300

    Add files via upload

---
+++ MainActivity2.java
@@ -0,0 +1,28 @@
+package com.Yusuf.Yusuf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.tobgui.R;
+
+public class MainActivity2 extends AppCompatActivity {
+    private Button button ;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+     }
+ }
+
+
+
+
+
